7114efbb16bee8b23252821bd7609ff6868d0e57
felipe <felipephplow@gmail.com>
2021-03-26 15:47:14 -0300

    Comitado favicon

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -7,6 +7,7 @@
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <script src="https://kit.fontawesome.com/0ab2bcde1c.js" crossorigin="anonymous"></script>
       <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link rel="shortcut icon" href="{{ url('assets/img/favicon.ico') }}" />
       <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet">
       <!-- Add the slick-theme.css if you want default styling -->
