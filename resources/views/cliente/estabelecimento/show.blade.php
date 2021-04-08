5a84d241c88cc35c48cb98bdae5a2fe679ff70ac
felipe <felipephplow@gmail.com>
2021-04-08 11:17:02 -0300

    Mais alt

--- resources/views/cliente/estabelecimento/show.blade.php
+++ resources/views/cliente/estabelecimento/show.blade.php
@@ -29,19 +29,19 @@
                               <td
                                     class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b block lg:table-cell relative lg:static">
                                     <span
-                                          class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Produto</span>
+                                          class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Nome</span>
                                     {{ $est->name }}
                               </td>
                               <td
                                     class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
                                     <span
-                                          class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Preço</span>
+                                          class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">WhatsApp</span>
                                     {{ $est->whatsapp }}
                               </td>
                               <td
                                     class="w-full lg:w-auto p-3 text-gray-800 text-center border border-b text-center block lg:table-cell relative lg:static">
                                     <span
-                                          class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Tempo
+                                          class="lg:hidden absolute top-0 left-0 bg-blue-200 px-2 py-1 text-xs font-bold uppercase">Telefone
                                           de Entrega</span>
                                     {{ $est->telefone }}
                               </td>
